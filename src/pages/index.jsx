ee9a81e0df7fe71f5a5f9549be2cac6e7c91208f
Samuel Adeboye Folaranmi <74235313+Boye95@users.noreply.github.com>
2023-01-23 21:44:30 +0100

    feat: added starter files for routes & pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,2 @@
+export { default as Login} from './login/login'
+export { default as Register} from './register/register'
---
+++ src/pages/register/register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './register.css'
+
+export default function Register() {
+  return (
+    <div className='container register'>
+        REgister
+    </div>
+  )
+}
