7c9b3f5a2fb6972837c994396f1521b19d442666
Samuel Adeboye Folaranmi <74235313+Boye95@users.noreply.github.com>
2023-01-25 14:10:47 +0100

    feat: restructured the file structure for auth

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,2 +1,2 @@
-export { default as Login} from './login/login'
-export { default as Register} from './register/register'
+// export { default as Login} from './login/login'
+// export { default as Register} from './register/register'
