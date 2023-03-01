a0cc7e5836adb6701a06d4d6fa8fb02530fc63e7
Samuel Adeboye Folaranmi <74235313+Boye95@users.noreply.github.com>
2023-03-01 19:46:25 +0100

    bug: relative path build issue

--- src/pages/dashboard/layout/layout.jsx
+++ src/pages/dashboard/layout/layout.jsx
@@ -1,7 +1,7 @@
 import layoutStyles from './layout.module.css'
 import {Outlet} from 'react-router-dom'
-import Topbar from '../components/topbar/Topbar'
-import Sidebar from '../components/sidebar/Sidebar'
+import Topbar from '../components/topbar/topbar'
+import Sidebar from '../components/sidebar/sidebar'
 
 
 export default function Layout() {
