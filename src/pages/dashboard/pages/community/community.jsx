20d866e1adce66cf1a885c848c3333d40128e852
Samuel Adeboye Folaranmi <74235313+Boye95@users.noreply.github.com>
2023-03-17 21:38:09 +0100

    question card styled

--- src/pages/dashboard/pages/community/community.jsx
+++ src/pages/dashboard/pages/community/community.jsx
@@ -1,8 +1,52 @@
 import communityStyle from './community.module.css'
 
+import Questioncard from './questioncard/questioncard';
 
-export default function Community() {
+{/* <ArrowDown size="32" color="#FF8A65"/> */}
+
+
+
+export default function Community () {
   return (
-    <div>community</div>
+    <div className={communityStyle.container}>
+      <div className={communityStyle.sectionOne}>
+        <div className={communityStyle.header}>
+          <div className={communityStyle.title}>
+            <h1 className={communityStyle.h1}>Community</h1>
+            <p className={communityStyle.p}>
+              Share what you know even and also ask about what you do not know
+            </p>
+          </div>
+          <button className={communityStyle.questionCta}>Ask Question</button>
+        </div>
+
+        <div className={communityStyle.questions}>
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+          <Questioncard />
+        </div>
+      </div>
+    </div>
   )
 }
---
+++ src/pages/dashboard/pages/community/questioncard/questioncard.jsx
@@ -0,0 +1,48 @@
+import questionCardStyles from './questioncard.module.css'
+import { ArrowDown, ArrowUp, ArchiveAdd, Edit } from 'iconsax-react'
+
+export default function Questioncard () {
+  return (
+    <div className={questionCardStyles.container}>
+      <div className={questionCardStyles.header}>
+        <h3 className={questionCardStyles.title}>
+          What is the difference between Product design and Ui/Ux?
+        </h3>
+        <div className={questionCardStyles.tags}>
+          <span className={questionCardStyles.tag}>UI/UX</span>
+          <span className={questionCardStyles.tag}>Design</span>
+        </div>
+      </div>
+
+      <div className={questionCardStyles.body}>
+        <div className={questionCardStyles.otherInfo}>
+          <div className={questionCardStyles.info}>
+            <span className={questionCardStyles.answerCount}>5 answers</span>
+            <span className={questionCardStyles.divider}>|</span>
+            <span className={questionCardStyles.timePosted}>
+              Last Request 12hrs ago
+            </span>
+          </div>
+
+          <div className={questionCardStyles.votes}>
+            <div className={questionCardStyles.upvotes}>
+              <ArrowUp size='19' className={questionCardStyles.icon} />
+              15
+            </div>
+            <div className={questionCardStyles.downvotes}>
+              <ArrowDown size='19' className={questionCardStyles.icon} />5
+            </div>
+            <div className={questionCardStyles.bookmark}>
+              <ArchiveAdd size='19' className={questionCardStyles.icon} />
+            </div>
+          </div>
+        </div>
+
+        <div className={questionCardStyles.answerBtn}>
+          <Edit size='19' className={questionCardStyles.editIcon} />
+          Answer
+        </div>
+      </div>
+    </div>
+  )
+}
