921c2d37572820861b82a19e0843184564d95652
Ayomide Shittu <coder6lvck@gmail.com>
2023-04-15 14:54:21 +0100

    update on account page

---
+++ src/pages/dashboard/pages/account/resetpassword/resetpass.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import resetStyles from './resetpass.module.css'
+
+export default function Resetpass() {
+  return (
+    <div className={resetStyles['container']}>
+        <div className={resetStyles['header']}>
+            <p>Reset Password</p>
+        </div>
+        <div className={resetStyles['body']}>
+            
+        </div>
+    </div>
+  )
+}
