0a115992868ba04d7bc826b039cf5f545d9c9fe6
Popoola Seyitan <60761852+Watermelon-Sugar@users.noreply.github.com>
2023-05-11 18:19:11 +0100

    changes to the mentor login ui

--- src/pages/auth/regularStudent/regularStudentRegister/regularStudentRegister.jsx
+++ src/pages/auth/regularStudent/regularStudentRegister/regularStudentRegister.jsx
@@ -1,165 +1,165 @@
-import React, { useState, useEffect } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
-import rglrStudRegStyle from './regularStudentRegister.module.css'
-import eyeIcon from '../../../../assets/general/eye.svg'
-import eyeClosedIcon from '../../../../assets/general/eyeclosed.svg'
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import rglrStudRegStyle from "./regularStudentRegister.module.css";
+import eyeIcon from "../../../../assets/general/eye.svg";
+import eyeClosedIcon from "../../../../assets/general/eyeclosed.svg";
 
-import { setUser } from '../../../../app/slices/generalSlices/userSlice'
+import { setUser } from "../../../../app/slices/generalSlices/userSlice";
 
-import { useDispatch } from 'react-redux'
+import { useDispatch } from "react-redux";
 
-import { useRegisterStudentMutation } from '../../../../app/slices/apiSlices/authSlice'
+import { useRegisterStudentMutation } from "../../../../app/slices/apiSlices/authSlice";
 
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
-export default function RegularStudentRegister () {
-  const [enterPassword, setEnterPassword] = useState(false)
-  const [toggleShowPassword, setToggleShowPassword] = useState(false)
+export default function RegularStudentRegister() {
+  const [enterPassword, setEnterPassword] = useState(false);
+  const [toggleShowPassword, setToggleShowPassword] = useState(false);
 
   // user data state
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [email, setEmail] = useState('')
-  const [matric, setMatric] = useState('')
-  const [track, setTrack] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [matric, setMatric] = useState("");
+  const [track, setTrack] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-  const [errorText, setErrorText] = useState('')
+  const [errorText, setErrorText] = useState("");
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [registerStudent, { data, isLoading, isSuccess, isError, error }] =
-    useRegisterStudentMutation()
+    useRegisterStudentMutation();
 
-  const handleStudentRegister = e => {
-    e.preventDefault()
+  const handleStudentRegister = (e) => {
+    e.preventDefault();
     if (password !== confirmPassword) {
-      setErrorText('Passwords do not match')
+      setErrorText("Passwords do not match");
     } else {
-      setErrorText('')
+      setErrorText("");
       registerStudent({
         firstname: firstName,
         lastname: lastName,
         email,
         password,
         track,
-        matric
-      })
+        matric,
+      });
     }
-  }
+  };
 
   useEffect(() => {
     if (isSuccess) {
       // console.log(data)
-      dispatch(setUser(data?.data))
-      navigate('/dashboard')
+      dispatch(setUser(data?.data));
+      navigate("/dashboard");
     }
     if (isError) {
-      setErrorText(error.data.message)
+      setErrorText(error.data.message);
     }
-  }, [isSuccess, isError])
+  }, [isSuccess, isError]);
 
   return (
     <div className={rglrStudRegStyle.container}>
       <h2 className={rglrStudRegStyle.header}>Register as an AltSchooler</h2>
       <form
-        action=''
+        action=""
         className={rglrStudRegStyle.form}
         onSubmit={handleStudentRegister}
       >
         <div
           className={`${rglrStudRegStyle.formGroup} ${rglrStudRegStyle.firstName}`}
         >
-          <label htmlFor='firstName'>First Name</label>
+          <label htmlFor="firstName">First Name</label>
           <input
-            type='text'
-            name='firstName'
-            id='firstName'
-            placeholder='Seun'
-            onChange={e => setFirstName(e.target.value)}
+            type="text"
+            name="firstName"
+            id="firstName"
+            placeholder="Seun"
+            onChange={(e) => setFirstName(e.target.value)}
             required
           />
         </div>
         <div
           className={`${rglrStudRegStyle.formGroup} ${rglrStudRegStyle.lastName}`}
         >
-          <label htmlFor='lastName'>Last Name</label>
+          <label htmlFor="lastName">Last Name</label>
           <input
-            type='text'
-            name='lastName'
-            id='lastName'
-            placeholder='Ogunlana'
-            onChange={e => setLastName(e.target.value)}
+            type="text"
+            name="lastName"
+            id="lastName"
+            placeholder="Ogunlana"
+            onChange={(e) => setLastName(e.target.value)}
             required
           />
         </div>
         <div
           className={`${rglrStudRegStyle.formGroup} ${rglrStudRegStyle.track}`}
         >
-          <label htmlFor=''>Track</label>
+          <label htmlFor="">Track</label>
           <select
-            name=''
-            id=''
-            onChange={e => setTrack(e.target.value)}
+            name=""
+            id=""
+            onChange={(e) => setTrack(e.target.value)}
             required
           >
-            <option value=''>Select Track</option>
-            <option value='backend engineering'>Backend Engineering</option>
-            <option value='cloud engineering'>Cloud Engineering</option>
-            <option value='data analysis'>Data Analysis</option>
-            <option value='data engineering'>Data Engineering</option>
-            <option value='data science'>Data Science</option>
-            <option value='frontend engineering'>Frontend Engineering</option>
-            <option value='product design'>Product Design</option>
-            <option value='product management'>Product Management</option>
-            <option value='product marketing'>Product Marketing</option>
+            <option value="">Select Track</option>
+            <option value="backend engineering">Backend Engineering</option>
+            <option value="cloud engineering">Cloud Engineering</option>
+            <option value="data analysis">Data Analysis</option>
+            <option value="data engineering">Data Engineering</option>
+            <option value="data science">Data Science</option>
+            <option value="frontend engineering">Frontend Engineering</option>
+            <option value="product design">Product Design</option>
+            <option value="product management">Product Management</option>
+            <option value="product marketing">Product Marketing</option>
           </select>
         </div>
         <div
           className={`${rglrStudRegStyle.formGroup} ${rglrStudRegStyle.matric}`}
         >
-          <label htmlFor='matric'>Altschool Student Number</label>
+          <label htmlFor="matric">Altschool Student Number</label>
           <input
-            type='text'
-            name='matric'
-            id='matric'
-            placeholder='ALT/NIN/2220'
-            onChange={e => setMatric(e.target.value)}
+            type="text"
+            name="matric"
+            id="matric"
+            placeholder="ALT/NIN/2220"
+            onChange={(e) => setMatric(e.target.value)}
             required
           />
         </div>
         <div
           className={`${rglrStudRegStyle.formGroup} ${rglrStudRegStyle.email}`}
         >
-          <label htmlFor='email'>Email Address</label>
+          <label htmlFor="email">Email Address</label>
           <input
-            type='email'
-            name='email'
-            id='email'
-            placeholder='seun@studybuddy.com'
-            onChange={e => setEmail(e.target.value)}
+            type="email"
+            name="email"
+            id="email"
+            placeholder="seun@studybuddy.com"
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
         </div>
         <div
           className={`${rglrStudRegStyle.formGroup} ${rglrStudRegStyle.password}`}
         >
-          <label htmlFor='password'>Password</label>
+          <label htmlFor="password">Password</label>
           <div className={rglrStudRegStyle.inputGroup}>
             <input
-              type={toggleShowPassword ? 'text' : 'password'}
-              name='password'
-              id='password'
-              placeholder=''
-              onChange={e => setPassword(e.target.value)}
+              type={toggleShowPassword ? "text" : "password"}
+              name="password"
+              id="password"
+              placeholder=""
+              onChange={(e) => setPassword(e.target.value)}
               required
             />
             <img
               src={toggleShowPassword ? eyeClosedIcon : eyeIcon}
-              alt='showPassword'
+              alt="showPassword"
               className={rglrStudRegStyle.showPasswordIcon}
               onClick={() => setToggleShowPassword(!toggleShowPassword)}
             />
@@ -168,19 +168,19 @@ export default function RegularStudentRegister () {
         <div
           className={`${rglrStudRegStyle.formGroup} ${rglrStudRegStyle.confirmPassword}`}
         >
-          <label htmlFor='confirmPassword'>Confirm Password</label>
+          <label htmlFor="confirmPassword">Confirm Password</label>
           <div className={rglrStudRegStyle.inputGroup}>
             <input
-              type={toggleShowPassword ? 'text' : 'password'}
-              name='confirmPassword'
-              id='confirmPassword'
-              placeholder=''
-              onChange={e => setConfirmPassword(e.target.value)}
+              type={toggleShowPassword ? "text" : "password"}
+              name="confirmPassword"
+              id="confirmPassword"
+              placeholder=""
+              onChange={(e) => setConfirmPassword(e.target.value)}
               required
             />
             <img
               src={toggleShowPassword ? eyeClosedIcon : eyeIcon}
-              alt='showPassword'
+              alt="showPassword"
               className={rglrStudRegStyle.showPasswordIcon}
               onClick={() => setToggleShowPassword(!toggleShowPassword)}
             />
@@ -195,13 +195,13 @@ export default function RegularStudentRegister () {
         )}
 
         <button
-          type='submit'
+          type="submit"
           className={rglrStudRegStyle.submitButton}
           disabled={isLoading}
         >
-          {isLoading ? 'Unlocking the door...' : 'Create Account'}
+          {isLoading ? "Unlocking the door..." : "Create Account"}
         </button>
       </form>
     </div>
-  )
+  );
 }
