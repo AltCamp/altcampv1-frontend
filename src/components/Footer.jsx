6e42b9ff3eca7c3543d2c834464e2503503db9ab
Popoola Seyitan <60761852+Watermelon-Sugar@users.noreply.github.com>
2023-05-22 11:03:39 +0100

    Revert "responsiveness of the lamding page"
    This reverts commit f47fcbe55d7ed4685899071f7f58c80881a390c7.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,91 +1,54 @@
-
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faGithubAlt,
-  faGoogle,
-  faFacebook,
-  faTwitter,
-  faLinkedin,
-  faFacebookF,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
-import { Facebook, Instagram, Twitch, Youtube } from "iconsax-react";
-import altcamplogo from "../assets/general/AuthWhiteLogo.svg";
-import mainFooterStyles from "./Footer.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubAlt, faGoogle, faFacebook, faTwitter, faLinkedin, faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { Facebook, Instagram, Twitch, Youtube } from 'iconsax-react'
+import altcamplogo from '../assets/general/AuthWhiteLogo.svg'
+import mainFooterStyles from './Footer.module.css'
 
 const Footer = () => {
-  return (
-    <footer>
-      <div className={mainFooterStyles.mainFooter}>
-        <div className={mainFooterStyles.footerHeader}>
-          <img src={altcamplogo} alt="logo" />
-          <div className={mainFooterStyles.smContainer}>
-            <div className={mainFooterStyles.smLinkContainer}>
-              <Youtube size="12" variant="Bold" />
+    return (
+        <footer className={mainFooterStyles['main-footer']}>
+            <div className={mainFooterStyles['']}>
+                <img src={altcamplogo} alt="" />
+                <div className={mainFooterStyles['sm-container']}>
+                    <div className={mainFooterStyles['sm-link-container']}>
+                        <Youtube size='12' variant="Bold" />
+                    </div>
+                    <div className={mainFooterStyles['sm-link-container']}>
+                    <FontAwesomeIcon icon={faLinkedin} />
+                    </div>
+                    <div className={mainFooterStyles['sm-link-container']}>
+                    <FontAwesomeIcon icon={faFacebookF} />
+                    </div>
+                    <div className={mainFooterStyles['sm-link-container']}>
+                    <FontAwesomeIcon icon={faInstagram} />
+                    </div>
+                    <div className={mainFooterStyles['sm-link-container']}>
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </div>
+                </div>
+                <p>© 2022 - 2023</p>
             </div>
-            <div className={mainFooterStyles.smLinkContainer}>
-              <FontAwesomeIcon icon={faLinkedin} />
+            <div className={mainFooterStyles['main-footer-list']}>
+                <h3>Product</h3>
+                <p>For AltSchoolers</p>
+                <p>For Instructors</p>
+                <p>For Mentors</p>
             </div>
-            <div className={mainFooterStyles.smLinkContainer}>
-              <FontAwesomeIcon icon={faFacebookF} />
+            <div className={mainFooterStyles['main-footer-list']}>
+                <h3>Features</h3>
+                <p>Feature</p>
+                <p>Feature</p>
+                <p>Feature</p>
             </div>
-            <div className={mainFooterStyles.smLinkContainer}>
-              <FontAwesomeIcon icon={faInstagram} />
+            <div className={mainFooterStyles['main-footer-list']}>
+                <h3>Company</h3>
+                <p>Company</p>
+                <p>Company</p>
+                <p>Company</p>
             </div>
-            <div className={mainFooterStyles.smLinkContainer}>
-              <FontAwesomeIcon icon={faTwitter} />
-            </div>
-          </div>
-          <p>© 2022 - 2023</p>
-        </div>
-        <div className={mainFooterStyles.mainFooterList}>
-          <div className={mainFooterStyles.footerList}>
-            {" "}
-            <h3>Product</h3>
-            <p>For AltSchoolers</p>
-            <p>For Instructors</p>
-            <p>For Mentors</p>
-          </div>
+        </footer>
+    )
+}
 
-          <div className={mainFooterStyles.footerList}>
-            {" "}
-            <h3>Features</h3>
-            <p>Feature</p>
-            <p>Feature</p>
-            <p>Feature</p>
-          </div>
-          <div className={mainFooterStyles.footerList}>
-            {" "}
-            <h3>Company</h3>
-            <p>Company</p>
-            <p>Company</p>
-            <p>Company</p>
-          </div>
-        </div>
-      </div>
-      <div className={mainFooterStyles.mobileContainer}>
-        <div className={mainFooterStyles.mobile}>
-          <div className={mainFooterStyles.smLinkContainer}>
-            <Youtube size="12" variant="Bold" />
-          </div>
-          <div className={mainFooterStyles.smLinkContainer}>
-            <FontAwesomeIcon icon={faLinkedin} />
-          </div>
-          <div className={mainFooterStyles.smLinkContainer}>
-            <FontAwesomeIcon icon={faFacebookF} />
-          </div>
-          <div className={mainFooterStyles.smLinkContainer}>
-            <FontAwesomeIcon icon={faInstagram} />
-          </div>
-          <div className={mainFooterStyles.smLinkContainer}>
-            <FontAwesomeIcon icon={faTwitter} />
-          </div>
-        </div>
-        <p>© 2022 - 2023</p>
-      </div>
-    </footer>
-  );
-};
 
-export default Footer;
+export default Footer
