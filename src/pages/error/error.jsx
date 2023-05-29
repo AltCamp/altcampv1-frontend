2149c1ba61d64aea643ab5629c05a905493af77c
Popoola Seyitan <60761852+Watermelon-Sugar@users.noreply.github.com>
2023-05-29 09:05:54 +0100

    error page fixed

--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import errorStyle from "./error.module.css";
-import ErrorImg from "../../assets/general/page-not-found.png";
+import ErrorImg from "../../assets/general/error-img.png";
 import { useRouteError } from "react-router-dom";
 export default function Error() {
   const error = useRouteError();
@@ -9,18 +9,22 @@ export default function Error() {
       <div className={errorStyle.error}>
         <div className={errorStyle.errorImg}>
           <img src={ErrorImg} alt="error" />
-          {/* <p>Error {error.status}</p> */}
+          <div className={errorStyle.errorStatus}>
+            <p className={errorStyle.errorName}>Error</p>
+            <p className={errorStyle.errorCode}>{error.status}</p>
+          </div>
         </div>
         <div className={errorStyle.errorText}>
-          <p>Page not found.</p>
-          Click{" "}
-          <a
-            className={errorStyle.errorLink}
-            href="https://github.com/AltCamp/altcampv1-frontend/issues/new/choose"
-          >
-            here{" "}
-          </a>
-          to report any issue.
+          <p>
+            Page not found. Click{" "}
+            <a
+              className={errorStyle.errorLink}
+              href="https://github.com/AltCamp/altcampv1-frontend/issues/new/choose"
+            >
+              here{" "}
+            </a>
+            to report any issue.
+          </p>
         </div>
       </div>
     </div>
