9712991f73cf2d53dc11d5fc4c540dc0be3c62f5
Esther Simon <esthersimon540@gmail.com>
2023-06-20 17:18:05 +0100

    sync

--- src/components/Toaster/Toaster.jsx
+++ src/components/Toaster/Toaster.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import style from "./Toaster.module.css";
-import { ErrorIcon, InfoIcon, WarningIcon, SuccessIcon } from '../../assets/toast'
+import {FaExclamationTriangle, FaCircleCheck,FaInfoCircle } from 'react-icons/fa'
+import {GrFormClose} from 'react-icons/gr'
+
 
 function Toaster({type, message, show }) {
     const icon = {
-        error : <ErrorIcon/>,
-        info : <InfoIcon/>,
-        warning : <WarningIcon/>,
-        success : <SuccessIcon/>,
+        error : <BsFillExclamationCircleFill/>,
+        info : <FaInfoCircle/>,
+        warning : <FaExclamationTriangle/>,
+        success : <FaCircleCheck/>,
     }
     return (
         <>
-         {show && <div className={(style.toast, style[type])} >
+         {show && <div className={`${style.toast} ${style[type]}`} >
             {icon[type]}
             <div>
                 <h6 className={style.toastTitle}>{type}</h6>
