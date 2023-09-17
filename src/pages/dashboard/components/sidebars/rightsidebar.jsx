0d79299ec0574de4d096c6426984784bbc917c52
Samuel Adeboye Folaranmi <74235313+Boye95@users.noreply.github.com>
2023-09-17 09:02:55 +0100

    fix: build error due to  top-level await

--- src/pages/dashboard/components/sidebars/rightsidebar.jsx
+++ src/pages/dashboard/components/sidebars/rightsidebar.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import client from '../../../../app/sanity/client-config';
 import contribute from '../../../../assets/general/contribute.png';
 import iconLogo from '../../../../assets/general/logonotext.svg';
@@ -12,8 +13,13 @@ import {
   useGetBackendRepoContributorsQuery,
 } from '../../../../app/slices/apiSlices/githubApiSlice';
 
-const data =
-  await client.fetch(`*[_type in ["post", "project"]] | order(_createdAt desc) [0...5] {
+export default function RightSidebar() {
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data =
+        await client.fetch(`*[_type in ["post", "project"]] | order(_createdAt desc) [0...5] {
   _id,
   title,
   "slug": slug.current,
@@ -28,9 +34,13 @@ const data =
   _createdAt
 }`);
 
-const posts = await data;
-
-export default function RightSidebar() {
+      const result = await data;
+      setPosts(result);
+    }
+    if (!posts) {
+      fetchPosts();
+    }
+  }, []);
   const {
     data: frontendData,
     isLoading: frontendLoading,
@@ -84,6 +94,29 @@ export default function RightSidebar() {
         <div className="flex w-full flex-col items-center gap-1 rounded-md bg-white p-2 shadow-lg">
           <Slider {...settings} className="w-full">
             <div className="!flex w-full flex-col items-center">
+              {frontendLoading && (
+                <div className="text-center">
+                  <div role="status">
+                    <svg
+                      aria-hidden="true"
+                      className="mr-2 inline h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                </div>
+              )}
               {frontendData && (
                 <div className="flex flex-col gap-2">
                   <a
@@ -157,17 +190,42 @@ export default function RightSidebar() {
                   </div>
                 </div>
               )}
-              <a
-                className="auth-btn flex w-fit items-center justify-center p-1 px-3 text-sm font-semibold"
-                href="https://github.com/AltCamp/altcampv1-frontend"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Get Started
-              </a>
+              {frontendData && (
+                <a
+                  className="auth-btn flex w-fit items-center justify-center p-1 px-3 text-sm font-semibold"
+                  href="https://github.com/AltCamp/altcampv1-frontend"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Get Started
+                </a>
+              )}
             </div>
 
             <div className="!flex w-full flex-col items-center">
+              {backendLoading && (
+                <div className="text-center">
+                  <div role="status">
+                    <svg
+                      aria-hidden="true"
+                      className="mr-2 inline h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                </div>
+              )}
               {backendData && (
                 <div className="flex flex-col gap-2">
                   <a
@@ -241,14 +299,16 @@ export default function RightSidebar() {
                   </div>
                 </div>
               )}
-              <a
-                className="auth-btn flex w-fit items-center justify-center p-1 px-3 text-sm font-semibold "
-                href="https://github.com/AltCamp/altcampv1-backend"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Get Started
-              </a>
+              {backendData && (
+                <a
+                  className="auth-btn flex w-fit items-center justify-center p-1 px-3 text-sm font-semibold "
+                  href="https://github.com/AltCamp/altcampv1-backend"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Get Started
+                </a>
+              )}
             </div>
 
             <div className="!flex w-full flex-col items-center">
